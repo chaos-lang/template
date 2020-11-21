f4eb33a8c268d423e1e57742ba2dfc59040af3b4
M. Mert Yildiran <me@mertyildiran.com>
2020-11-21 04:20:20 +0300

    Add arguments to defineFunction() calls for specifying secondary type

--- template.c
+++ template.c
@@ -2,6 +2,7 @@
 
 char *hello_params_name[] = {};
 unsigned hello_params_type[] = {};
+unsigned hello_params_secondary_type[] = {};
 unsigned short hello_params_length = 0;
 int KAOS_EXPORT Kaos_hello()
 {
@@ -13,7 +14,7 @@ int KAOS_EXPORT Kaos_hello()
 int KAOS_EXPORT KaosRegister(struct Kaos _kaos)
 {
     kaos = _kaos;
-    kaos.defineFunction("hello", K_VOID, hello_params_name, hello_params_type, hello_params_length);
+    kaos.defineFunction("hello", K_VOID, K_ANY, hello_params_name, hello_params_type, hello_params_secondary_type, hello_params_length, NULL, 0);
 
     return 0;
 }
